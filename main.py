b38b90ed56fa2a699e8c968f825525b1a0358eb3
Abdurauf2004 <156092100+Abdurauf2004@users.noreply.github.com>
2024-01-09 15:06:03 +0500

    dasturim
    Birinchi_dasturim

---
+++ main.py
@@ -0,0 +1,54 @@
+# Birinchi dasturimiz.
+# 19:46:30
+# Muallif:Abdurauf Islomjonov
+
+# print("Hello World!")
+
+# 1.Ixtiyoriy o’zgaruvchiga ismingiz va yoshingizni yuklang!
+# a="Islomjonpov Abdurauf 19 yoshda"
+# print(a)
+
+# 2."Hello World!" matnini yangi o'zgaruvchiga yuklang va print() yordamida konsolga chiqaring
+# a="Hello World!"
+# print(a)
+
+# 3.Ixtiyoriy o’zgaruvchiga ismingiz va yoshingizni yuklang
+# hamda konsolga “Salom,mening ismim ...! Men ... yoshdaman” degan yozuvni chiqaring.
+# a="Abdurauf"
+# b=19
+# print("Mening ismim", a,"!", "Men", b, "yoshdaman")
+
+# 4.Foydalanuvchi tomonidan ismi hamda familiyasi input() orqali kiritilsin
+# va konsolga “Assalomu alaykum,mening ism familiyam ... ...”  degan yozuv chiqsin!
+# a=str(input("Familyangiz va Ismingizni kiriting->")).title()
+# print("Assalomu alaykum,mening ism familiyam", a)
+
+# 5.Xabar deb nomlangan o'zgaruvchiga biror matn yuklang va konsolga chiqaring
+# keyin esa o'zgaruvchiga yangi qiymat berib uni ham konsolga chiqaring.
+# xabar="Marketing va reklama: Veb-sayt marketing kampaniyalari, reklama aktsiyalari va yangi mahsulotlar yoki voqealar haqida yangiliklarni taqdim etish uchun ishlatilishi mumkin. "
+# print(xabar)
+
+# 6.string,integer,float,boolean tipiga oid qiymatlarni o’zgaruvchilarga tenglang!
+# a="Hello"
+# b=12
+# c=12.1
+# d=bool
+
+# 7.False deb nomlangan o'zgaruvchi yarating,
+# unga biror qiymat bering va konsolga chiqaring (siz kutgan natija chiqdimi?)
+# False=12
+# Bunday o`zgaruvchini ishlatib bo`lmaydi.
+
+# 8.	5 ta float turidagi o’zgaruvchi yozing, 2 tasining qiymatini foydalanuvchi kiritsin(inputdan foydalaning).
+# a=12.1
+# b=23.98
+# c=245.5
+# d=float(input("Son kiriting->"))
+# f=float(input("Son kiriting->"))
+# print(a, b, c, d, f)
+
+# 10.Foydalanuvchining tug’ilgan yilini input qilib oling.
+# O’sha o’zgaruvchiga o’zingizning tug’ilgan yilingizni yangi qiymat qilib yozing va uni ekranga chiqaring.
+# a=input("Tug`ilgan yilingizni kiriting->")
+# print(a)
+# print(3+5-6*9//5*4-5+4-5+445+-5+55//45+5-6)
